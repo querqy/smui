237ecac7995439611159476db0977c312f0f17fe
Paul Maria Bartusch <paulbartusch@gmx.de>
2019-01-18 09:00:55 +0100

    v2.0.0 - fix for PR#6 (@Inject for HealthController)

--- app/controllers/HealthController.scala
+++ app/controllers/HealthController.scala
@@ -1,11 +1,11 @@
 package controllers
 
+import javax.inject.Inject
 import play.api.libs.json.Json
 import play.api.mvc._
 
 
-class HealthController(cc: ControllerComponents)
-  extends AbstractController(cc) {
+class HealthController @Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
 
   val health = Action {
     Ok(Json.parse(models.buildInfo.BuildInfo.toJson))
