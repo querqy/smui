20e6aa96895a783821e5d93dc4ac614c74895c85
Paul Maria Bartusch <paulbartusch@gmx.de>
2020-08-12 14:26:07 +0200

    component skeleton to split header-nav from app root component

---
+++ app/assets/app/components/header-nav/header-nav.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ViewChild, SimpleChanges } from '@angular/core';
+import {FeatureToggleService, SolrService} from '../services/index';
+
+@Component({
+  selector: 'smui-header-nav',
+  templateUrl: './header-nav.component.html',
+  styleUrls: ['./header-nav.component.css']
+})
+export class HeaderNavComponent implements OnInit {
+
+
+
+}
