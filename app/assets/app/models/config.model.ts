414537687df52bf740d8fb80b0417d235c9aad98
Paul Maria Bartusch <paulbartusch@gmx.de>
2020-09-07 08:05:55 +0200

    interface with latestVersionInfo on frontend

---
+++ app/assets/app/models/config.model.ts
@@ -0,0 +1,6 @@
+export class SmuiVersionInfo {
+  latestMarketStandard?: string
+  current?: string
+  infoType: string
+  msg: string
+}
--- app/assets/app/services/report.service.ts
+++ app/assets/app/services/report.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
-import { RulesReport, ActivityReport	 } from '../models/index';
+import { RulesReport, ActivityReport } from '../models/index';
 
 @Injectable()
 export class ReportService {
