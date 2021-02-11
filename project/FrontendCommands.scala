f202f468abd5077e359f8227967ec7f4c6a7f15c
Gunnar Busch <gunnar.busch@qudosoft.de>
2021-02-11 17:16:05 +0100

    Add --legacy-peer-deps to solve peer dependecy problems

--- project/FrontendCommands.scala
+++ project/FrontendCommands.scala
@@ -1,5 +1,5 @@
 object FrontendCommands {
-  val dependencyInstall: String = "npm install"
+  val dependencyInstall: String = "npm install --legacy-peer-deps"
   val test: String = "npm run test"
   val serve: String = "npm run start"
   val build: String = "npm run build:prod"
