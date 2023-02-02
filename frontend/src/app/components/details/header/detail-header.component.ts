1e6613cb685da99cfc95a4b34acc9979bf89c6aa
Paul M. Bartusch <paulbartusch@gmx.de>
2023-02-02 18:35:07 +0100

    Warn users about exact matching syntax for correct spellings.

--- frontend/src/app/components/details/header/detail-header.component.ts
+++ frontend/src/app/components/details/header/detail-header.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class DetailHeaderComponent {
   @Input() title = '';
+  @Input() exactMatchWarn = false;
   @Input() placeholder = 'Please enter...';
   @Input() term = '';
   @Input() disabled = false;
@@ -16,4 +17,13 @@ export class DetailHeaderComponent {
   @Output() activeChange = new EventEmitter();
   @Output() handleSave = new EventEmitter();
   @Output() handleDelete = new EventEmitter();
+
+  warn_for_exactmatch(): boolean {
+    if(!this.exactMatchWarn) {
+      return false
+    } else {
+      const trimmedTerm = this.term.trim()
+      return trimmedTerm.startsWith('"') && trimmedTerm.endsWith('"')
+    }
+  }
 }
