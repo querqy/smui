6add0f0e398b756e53f9c93505c86d70f4cce99b
dobestler <dbestler@gmail.com>
2023-02-21 11:07:50 +0100

    unit tests for copy functions of rules

---
+++ test/models/rules/DeleteRuleSpec.scala
@@ -0,0 +1,21 @@
+package models.rules
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class DeleteRuleSpec extends FlatSpec with Matchers {
+
+  val rule1 = DeleteRule(DeleteRuleId(), "term1", isActive = true)
+  val rule2 = DeleteRule(DeleteRuleId(), "term2", isActive = false)
+
+  "DeleteRule" should "create copy that has same values and a new Id" in {
+    assertValues(rule1, DeleteRule.createWithNewIdFrom(rule1))
+    assertValues(rule2, DeleteRule.createWithNewIdFrom(rule2))
+  }
+
+  def assertValues(expected: DeleteRule, actual: DeleteRule): Unit = {
+    actual.id should not be expected.id
+    actual.term shouldBe expected.term
+    actual.isActive shouldBe expected.isActive
+  }
+
+}
